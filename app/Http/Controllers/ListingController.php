ce89885b4c14e4e9c0fe75fdc4e669b99a24c4ed
Ariful/ITP/Ariful Islam (Email: ariful.islam@reddotdigitalit.com) <ariful.islam@reddotdigitalit.com>
2022-09-26 00:25:14 +0600

    sixth commit

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,22 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
-    // Show all listings
+    // Show All Listings
     public function index()
     {
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
-    // Show single listing
+    // Show Single Listing
     public function show(Listing $listing)
     {
         return view('listings.show', [
             'listing' => $listing
         ]);
+    }
+
+    // Show create form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,11 @@ use Illuminate\Support\Facades\Route;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
